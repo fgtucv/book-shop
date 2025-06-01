3756d1968091c02d733d92dfc3225308079a0d1d
Oles Slava <oles.slava23@gmail.com>
2025-06-01 15:33:39 +0300

    fix phone modal

--- src/index-JS/modal/phoneModal.js
+++ src/index-JS/modal/phoneModal.js
@@ -2,8 +2,12 @@ const openButton = document.querySelector(".header__menu-button");
 const closeButton = document.querySelector(".phone__close-button");
 const bacdrop = document.querySelector(".phone-bacdrop");
 
-openButton.addEventListener("click", openModal);
-closeButton.addEventListener("click", closeModal);
+if (JSON.parse(localStorage.getItem("status")) === "login") {
+    openButton.addEventListener("click", openModal);
+    closeButton.addEventListener("click", closeModal);
+} else {
+    return;
+}
 
 function openModal() {
     bacdrop.classList.remove("is-hidden");
